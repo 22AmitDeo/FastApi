b9a71eb812d49614a9aa139bb7183702574a4031
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-04-02 18:18:29 +0530

    #

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ def read_root():
 def about():
     return {"data":{"About":"Made by Amit"}}
 
+#Path Parameter 
 @app.get("/blog/{id}")
 def getid(id:int):
     return{"data":id}
+
+#Query parameter 
+@app.get("/items/")
+def get_items(name: str, price: float = None):
+    return {"name": name, "price": price}
